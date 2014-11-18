d714a7228087114d173cb5f8bfdd3aba6672e2de
Panin Osei <panin.tenkorang@meltwater.org>
2014-11-18 13:13:12 +0000

    Improved side bar and bottom footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+/*
+ * Bootstrap v3.3.0 (http://getbootstrap.com)
+ * Copyright 2011-2014 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
